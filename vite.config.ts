686b3bab5fea6d9c95e8cebac07500b2ccaabcec
ariburaco <a.burak@softavail.com>
2023-05-17 00:31:01 +0300

    removed background.js

--- vite.config.ts
+++ vite.config.ts
@@ -42,9 +42,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         content: resolve(pagesDir, "content", "index.ts"),
-        background: resolve(pagesDir, "background", "index.ts"),
         contentStyle: resolve(pagesDir, "content", "style.scss"),
-        // popup: resolve(pagesDir, "popup", "index.html"),
       },
       watch: {
         include: ["src/**", "vite.config.ts"],
